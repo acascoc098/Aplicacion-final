3d40ff7c9bc836b44cb315307c21b1242f2b00dd
acascoc098 <acascoc098@g.educaand.es>
2023-10-27 10:43:22 +0200

    Creación del método para cambiar de actividad.

--- app/src/main/java/com/example/aplicacionfinal/MainActivity.kt
+++ app/src/main/java/com/example/aplicacionfinal/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.aplicacionfinal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnLlamada : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initEvent();
+    }
+
+    private fun initEvent(){
+        btnLlamada = findViewById(R.id.button);
+
+        btnLlamada.setOnClickListener { view ->
+            intent = Intent(this, Llamada::class.java)
+            startActivity(intent)
+        }
     }
 }
