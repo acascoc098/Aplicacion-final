bc4e1a273334206a2af0c74a31a8180e5a4df3a4
acascoc098 <acascoc098@g.educaand.es>
2023-10-28 00:13:00 +0200

    Primer intento de botón para sitio web.

--- app/src/main/java/com/example/aplicacionfinal/MainActivity.kt
+++ app/src/main/java/com/example/aplicacionfinal/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.aplicacionfinal
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var btnLlamada : Button
+    private lateinit var btnURL : Button
+    private lateinit var url : String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,6 +23,19 @@ class MainActivity : AppCompatActivity() {
         btnLlamada.setOnClickListener { view ->
             intent = Intent(this, Llamada::class.java)
             startActivity(intent)
+        }
+
+        btnURL = findViewById(R.id.button6)
+
+        btnURL.setOnClickListener {
+            val url = "https://www.google.com/?hl=es"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            //startActivity(intent)
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent)
+            }else{
+                Toast.makeText( this, "No es posible lanzar la url del sitio web", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
