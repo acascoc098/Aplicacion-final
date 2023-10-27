6eebf2d144f8e084708f1485a44bc8077bb194d7
acascoc098 <acascoc098@g.educaand.es>
2023-10-27 10:56:13 +0200

    Creación botón para volver desde la llamada al menú principal.

--- app/src/main/java/com/example/aplicacionfinal/Llamada.kt
+++ app/src/main/java/com/example/aplicacionfinal/Llamada.kt
@@ -1,4 +1,25 @@
 package com.example.aplicacionfinal
 
-class Llamada {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class Llamada : AppCompatActivity() {
+    private lateinit var btnVolver : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.llamada_layout)
+        initEvent()
+    }
+
+    private fun initEvent(){
+        btnVolver = findViewById(R.id.button5);
+
+        btnVolver.setOnClickListener { view ->
+            intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
