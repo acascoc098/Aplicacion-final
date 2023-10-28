25bd720b2e0ac563b69f6f26f1dfdfb2eb3a5159
acascoc098 <acascoc098@g.educaand.es>
2023-10-28 19:57:51 +0200

    Versión final, añadiendo el botón de alarma.

--- app/src/main/java/com/example/aplicacionfinal/Llamada.kt
+++ app/src/main/java/com/example/aplicacionfinal/Llamada.kt
@@ -1,10 +1,12 @@
 package com.example.aplicacionfinal
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
@@ -12,30 +14,26 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 
 class Llamada : AppCompatActivity() {
-    private lateinit var btnVolver : Button
-    private lateinit var btnLlamar : Button
-
     companion object{
         const val NUMERO = "697105928"
     }
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.llamada_layout)
-        initEvent()
-    }
 
-    private fun initEvent(){
-        btnVolver = findViewById(R.id.button5)
-        btnLlamar = findViewById(R.id.button4)
-
+        var btnVolver = findViewById<Button>(R.id.button5)
         btnVolver.setOnClickListener { view ->
-            intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
 
-        btnLlamar.setOnClickListener {
-            requestPermissions()
+        var btnLlamar = findViewById<Button>(R.id.button4)
+        btnLlamar.setOnClickListener { view ->
+            if (view.id==R.id.button4) {
+                requestPermissions()
+            }
         }
     }
 
@@ -44,7 +42,7 @@ class Llamada : AppCompatActivity() {
             if (PermissionPhone()){
                 call()
             }else{
-                requestPermissionLauncher.launch(android.Manifest.permission. CALL_PHONE)
+                requestPermissionLauncher.launch(android.Manifest.permission.CALL_PHONE)
             }
         }else{
             call()
@@ -52,24 +50,25 @@ class Llamada : AppCompatActivity() {
     }
 
     private fun call() {
-        val intent = Intent(Intent.ACTION_CALL).apply {
-            data = Uri.parse("tel:$NUMERO")
+        val intent = Intent(Intent. ACTION_CALL).apply {
+            data = Uri.parse( "tel:${Companion.NUMERO}")
         }
         startActivity(intent)
     }
 
     private fun PermissionPhone(): Boolean =
         ContextCompat.checkSelfPermission( this,
-            android.Manifest.permission. CALL_PHONE) == PackageManager. PERMISSION_GRANTED
+            android.Manifest.permission.CALL_PHONE) == PackageManager. PERMISSION_GRANTED
 
-    private val requestPermissionLauncher =  registerForActivityResult(
+    private val requestPermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestPermission()) {
             isGranted->
-                if (isGranted) {
-                    call()
-                }
-                else {
-                    Toast.makeText( this, "Necesitas habilitar los permisos", Toast.LENGTH_LONG).show()
-                }
+        if (isGranted) {
+            call()
         }
+        else {
+            Toast.makeText( this, "Necesitas habilitar los permisos",
+                Toast.LENGTH_LONG).show()
+        }
+    }
 }
--- app/src/main/java/com/example/aplicacionfinal/MainActivity.kt
+++ app/src/main/java/com/example/aplicacionfinal/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
 import android.widget.Button
 import android.widget.Toast
 
@@ -11,46 +12,56 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btnLlamada : Button
     private lateinit var btnURL : Button
     private lateinit var btnMaps : Button
+    private lateinit var btnAlarma : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initEvent();
-    }
-
-    private fun initEvent(){
         btnLlamada = findViewById(R.id.button);
 
         btnLlamada.setOnClickListener { view ->
-            intent = Intent(this, Llamada::class.java)
+            val intent = Intent(this, Llamada::class.java)
             startActivity(intent)
         }
 
         btnURL = findViewById(R.id.button6)
 
         btnURL.setOnClickListener {view ->
-            val url = "https://www.google.com/?hl=es"
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-            //startActivity(intent)
-            if (intent.resolveActivity(packageManager) != null) {
-                startActivity(intent)
-            }else{
-                Toast.makeText( this, "No es posible lanzar la url del sitio web", Toast.LENGTH_LONG).show()
-            }
+            val url = "https://www.twitch.tv"
+            val uri = Uri.parse(url)
+            val intent2 = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent2)
+
         }
 
         btnMaps = findViewById(R.id.button2)
 
         btnMaps.setOnClickListener {view ->
-            val latitude = 37.9214200 // Latitud
-            val longitude = -2.9788000 // Longitud
+            val latitude = 37.9214200 // Coordenad de Cazorla, pero no son exactas
+            val longitude = -2.9788000
             val gmmIntentUri = Uri.parse("geo:$latitude,$longitude")
             val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
 
             mapIntent.setPackage("com.google.android.apps.maps")
 
-            if (mapIntent.resolveActivity(packageManager) != null) {
+            if (mapIntent.resolveActivity(packageManager) == null) {
                 startActivity(mapIntent)
+            }else{
+                Toast.makeText( this, "Error al abrir Google Maps",
+                    Toast.LENGTH_LONG).show()
             }
+        }
+
+        btnAlarma = findViewById(R.id.button3)
+
+        btnAlarma.setOnClickListener { view ->
+            val intent3 = Intent(AlarmClock.ACTION_SET_ALARM).apply {
+                putExtra(AlarmClock.EXTRA_MESSAGE, "Despierta")
+                putExtra(AlarmClock.EXTRA_HOUR, 7)
+                putExtra(AlarmClock.EXTRA_MINUTES, 38)
+            }
+
+            startActivity(intent3)
+
         }
     }
 }
